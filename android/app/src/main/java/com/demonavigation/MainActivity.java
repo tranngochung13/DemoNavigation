e1596456b7a339e07d8c325fc8d8018d6cf69878
Tran Ngoc Hung <tranngochung1302@gmail.com>
2020-08-20 18:39:05 +0700

    config navigation

--- android/app/src/main/java/com/demonavigation/MainActivity.java
+++ android/app/src/main/java/com/demonavigation/MainActivity.java
@@ -1,14 +1,14 @@
 package com.demonavigation;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
+  // @Override
   protected String getMainComponentName() {
     return "DemoNavigation";
   }
